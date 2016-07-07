bbf4de4635be3a93b6efcbbac1e813d81cc8a3b5
Lee Seung-jin <fewfree12@naver.com>
2016-07-07 10:20:59 +0900

    인벤토리 구현 시작 아이템 버튼 생성 및 UI 구현

---
+++ Inventory/Inventory/Assets/Scripts/InventoryButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryButton : MonoBehaviour {
+    private GameObject inventoryButtonPanel;
+
+	// Use this for initialization
+	void Start () {
+        inventoryButtonPanel = GameObject.FindGameObjectWithTag("InventoryButtonPanel");
+
+        //버튼이 생성될 때 버튼 리스트에 추가
+        inventoryButtonPanel.GetComponent<InventoryButtonArray>().inventoryButtonList.Add(this.gameObject);
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+	}
+}
---
+++ Inventory/Inventory/Assets/Scripts/InventoryButtonArray.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryButtonArray : MonoBehaviour {
+    public List<GameObject> inventoryButtonList = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+	}
+}
